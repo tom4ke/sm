5304a07263df895e5d680cd51be63ab5cc23fa30
TomAke <admin@MacBook-Pro-Admin.local>
2020-02-11 22:01:31 +0600

    Initial commit

--- sm/settings.py
+++ sm/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from local_config import *
-SECRET_KEY = local_config.SECRET_KEY
+from secrets import *
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
